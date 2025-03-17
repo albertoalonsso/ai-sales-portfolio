0ad94d43326b6aba65662c7433ec347c3fc4723f
albertoalonsso <albertoalonsozips@gmail.com>
2025-03-17 12:21:57 +0100

    control

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -77,7 +77,7 @@ const Hero = () => {
           </Button>
         </div>
         
-        {/* Technology Pills*/}
+        {/* Technology Pills g*/}
         <div className="flex flex-wrap justify-center gap-3 mt-8 animate-fade-in" style={{ animationDelay: '0.6s' }}>
           <div className="pill">Artificial Intelligence</div>
           <div className="pill">Deep Learning</div>
